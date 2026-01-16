52be65f5010a053b672ba1be3205411c2ff7581c
Vaibhav Sharma <asvaibhavsharma@gmail.com>
2026-01-16 13:00:05 +0530

    history fix

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,13 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import DashboardView from '../views/DashboardView.vue'
 import FundDetailView from '../views/FundDetailView.vue'
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'dashboard', component: DashboardView },
-    { path: '/fund/:schemeCode', name: 'fund', component: FundDetailView, props: true },
+    {
+      path: '/fund/:schemeCode',
+      name: 'fund',
+      component: FundDetailView,
+      props: true,
+    },
     { path: '/:pathMatch(.*)*', redirect: '/' },
   ],
   scrollBehavior() {
